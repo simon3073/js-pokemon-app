af2cee0ea3f141c2b0a6d15a587e6866ff16d4ad
simon3073 <simonbertoli@gmail.com>
2022-04-05 16:00:06 +1000

    Achievement 1 - Submission 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
-alert('Hello World');
-
-let favoriteFood = 'Everything';
-document.write(favoriteFood);
+// Array of Pokemon data to display in the application
+let pokemonList = [
+	{
+		name: 'Ivysaur',
+		height: 1,
+		types: ['grass', 'poison']
+	},
+	{
+		name: 'Ekans',
+		height: 2,
+		types: ['poison']
+	},
+	{
+		name: 'Fearow',
+		height: 1.2,
+		types: ['flying', 'normal']
+	}
+];
