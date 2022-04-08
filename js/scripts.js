9e61dd5006b62a3e9ab0763553eba9acaac11b0a
simon3073 <simonbertoli@gmail.com>
2022-04-08 13:30:51 +1000

    Exercise 1.5 - Task 2 Submission

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,41 @@
-// Array of Pokemon data to display in the application
-let pokemonList = [
-	{
-		name: 'Ivysaur',
-		height: 1,
-		types: ['grass', 'poison']
-	},
-	{
-		name: 'Ekans',
-		height: 2,
-		types: ['poison']
-	},
-	{
-		name: 'Fearow',
-		height: 1.2,
-		types: ['flying', 'normal']
+let pokemonRepository = (function () {
+	// Array of Pokemon data to display in the application
+	let pokemonList = [
+		{
+			name: 'Ivysaur',
+			height: 1,
+			types: ['grass', 'poison']
+		},
+		{
+			name: 'Ekans',
+			height: 2,
+			types: ['poison']
+		},
+		{
+			name: 'Fearow',
+			height: 1.2,
+			types: ['flying', 'normal']
+		}
+	];
+
+	function getAll() {
+		return pokemonList;
 	}
-];
+
+	function add() {
+		return pokemonList;
+	}
+
+	return {
+		getAll,
+		add
+	};
+})();
 
 const pokemonDiv = document.getElementById('output');
 
 // Loop through the Pokemon Object Array and add output to the websites DOM
-pokemonList.forEach((pokemon) => {
+pokemonRepository.getAll().forEach((pokemon) => {
 	pokemonDiv.innerHTML += `<b>${pokemon.name} (Height: ${pokemon.height})</b>`;
 	// If pokemon height above 1.8 - add a special message
 	if (pokemon.height > 1.8) pokemonDiv.innerHTML += ` <i>${pokemon.name} is a big Pokemon!</i>`;
